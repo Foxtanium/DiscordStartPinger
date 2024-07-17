83d6aaf3a6affbead925f92c3be2dc3a020cd465
FuchsFuchs1 <70401056+Fjox@users.noreply.github.com>
2024-07-17 13:13:58 +0200

    added the tracking id

--- src/main/java/org/bstats/bstats.java
+++ src/main/java/org/bstats/bstats.java
@@ -7,7 +7,7 @@ public class ExamplePlugin extends Plugin {
     public void onEnable() {
         // All you have to do is adding the following two lines in your onEnable method.
         // You can find the plugin ids of your plugins on the page https://bstats.org/what-is-my-plugin-id
-        int pluginId = 1234; // <-- Replace with the id of your plugin!
+        int pluginId = 22673; // <-- Replace with the id of your plugin!
         Metrics metrics = new Metrics(this, pluginId);
 
         // Optional: Add custom charts
