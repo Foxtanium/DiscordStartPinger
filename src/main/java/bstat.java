198678bd6c4bf53499cc479adb1ee253782a0e62
FuchsFuchs1 <70401056+Fjox@users.noreply.github.com>
2024-07-17 13:11:57 +0200

    nvm

---
+++ src/main/java/bstat.java
@@ -0,0 +1,17 @@
+import net.md_5.bungee.api.plugin.Plugin;
+import org.bstats.bungeecord.Metrics;
+
+public class ExamplePlugin extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // All you have to do is adding the following two lines in your onEnable method.
+        // You can find the plugin ids of your plugins on the page https://bstats.org/what-is-my-plugin-id
+        int pluginId = 1234; // <-- Replace with the id of your plugin!
+        Metrics metrics = new Metrics(this, pluginId);
+
+        // Optional: Add custom charts
+        metrics.addCustomChart(new Metrics.SimplePie("chart_id", () -> "My value"));
+    }
+
+}
